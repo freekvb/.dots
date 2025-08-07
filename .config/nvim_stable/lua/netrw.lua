7d89832ee0bd8b33d07c9d7318ff503792cbc3de
freekvb <freekvb@gmail.com>
2025-08-07 05:29:44 +0200

    add

---
+++ .config/nvim_stable/lua/netrw.lua
@@ -0,0 +1,68 @@
+------------------------------------------------------------------------------
+-- File:     ~/.config/nvim/lua/netrw.lua (archlinux @ 'silent')
+-- Date:     Fri 14 Jul 2025 06:30
+-- Update:   Wed 30 Jul 2025 21:15
+-- Owner:    fvb - freekvb@gmail.com - https://freekvb.github.io/fvb/
+-------------------------------------------------------------------------------
+
+---- netrw ----
+
+-- setttings
+vim.g.netrw_keepdir = 0
+vim.g.netrw_winsize = -44
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 4
+vim.g.netrw_browse_split = 4
+vim.g.netrw_use_errorwindow = 1
+vim.g.netrw_bufsettings = "noma nu nowrap ro"
+vim.g.netrw_bookmark = 1
+
+-- open in split to the right
+vim.cmd([[
+function! OpenToRight()
+	:normal v
+	let g:path=expand('%:p')
+	execute 'q!'
+	execute 'belowright vnew' g:path
+	:normal <c-w>l
+endfunction
+]])
+
+-- open in split below
+vim.cmd([[
+function! OpenBelow()
+	:normal v
+	let g:path=expand('%:p')
+	execute 'q!'
+	execute 'belowright new' g:path
+	:normal <c-w>l
+endfunction
+]])
+
+-- keymapping function
+vim.cmd([[
+function! NetrwMappings()
+    " Hack fix to make ctrl-l work properly
+    noremap <buffer> <c-l> <c-w>l
+    " make h and l work as intended
+    nmap <buffer> h u
+    nmap <buffer> l <cr>
+    " close netrw
+    nmap <buffer> <Leader>dd :Lexplore<CR>
+endfunction
+augroup netrw_mappings
+	autocmd!
+	autocmd filetype netrw call NetrwMappings()
+augroup END
+]])
+
+-- close hidden buffer
+vim.cmd([[
+    autocmd FileType netrw setl bufhidden=delete
+]])
+
+-- close netrw if it's the only buffer open
+vim.cmd([[
+    autocmd WinEnter * if winnr('$') == 1 && getbufvar(winbufnr(winnr()), "&filetype") == "netrw" || &buftype == 'quickfix' |q|endif
+]])
+
---
+++ .config/nvim_stable/lua/plugins.lua
@@ -0,0 +1,96 @@
+------------------------------------------------------------------------------
+-- File:     ~/.config/nvim/lua/plugins.lua (archlinux @ 'silent')
+-- Date:     Fri 14 Jul 2025 06:30
+-- Update:   Wed 30 Jul 2025 21:21
+-- Owner:    fvb - freekvb@gmail.com - https://freekvb.github.io/fvb/
+-------------------------------------------------------------------------------
+
+---- plugins ----
+
+-- lualine
+require("lualine").setup({
+    options = {
+        icons_enabled = false,
+        theme = "auto",
+        component_separators = "",
+        section_separators = "",
+    },
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff" },
+        lualine_c = { "filename" },
+        lualine_x = {
+            function()
+                local encoding = vim.o.fileencoding
+                if encoding == "" then
+                    return vim.bo.fileformat .. " :: " .. vim.bo.filetype
+                else
+                    return encoding .. " :: " .. vim.bo.fileformat .. " :: " .. vim.bo.filetype
+                end
+            end,
+        },
+        lualine_y = { "filesize", "progress" },
+        lualine_z = { "location" },
+    },
+})
+
+-- mini starter
+require('mini.starter').setup({
+    autoopen = true,
+    evaluate_single = false,
+    items = nil,
+    header = " ",
+    footer = " ",
+    content_hooks = nil,
+    query_updaters = 'abcdefghijklmnopqrstuvwxyz0123456789_-.',
+    silent = false,
+})
+
+-- smear cursor
+require('smear_cursor').setup({
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+    scroll_buffer_space = true,
+    legacy_computing_symbols_support = false,
+    smear_insert_mode = true,
+    -- options                            -- default  range
+    stiffness = 0.8,                      -- 0.6      [0, 1]
+    trailing_stiffness = 0.5,             -- 0.4      [0, 1]
+    stiffness_insert_mode = 0.7,          -- 0.5      [0, 1]
+    trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+    damping = 0.8,                        -- 0.65     [0, 1]
+    damping_insert_mode = 0.8,            -- 0.7      [0, 1]
+    distance_stop_animating = 0.5,        -- 0.1      > 0
+})
+
+-- colorscheme
+require("black-metal").setup({
+  -- theme: bathory | burzum | dark-funeral | darkthrone | emperor | gorgoroth | immortal | impaled-nazarene | khold | marduk | mayhem | nile | taake | thyrfing | venom | windir
+  theme = "mayhem",
+})
+require("black-metal").load()
+
+-- pckr
+local function bootstrap_pckr()
+  local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
+  if not (vim.uv or vim.loop).fs_stat(pckr_path) then
+    vim.fn.system({
+      'git',
+      'clone',
+      "--filter=blob:none",
+      'https://github.com/lewis6991/pckr.nvim',
+      pckr_path
+    })
+  end
+  vim.opt.rtp:prepend(pckr_path)
+end
+bootstrap_pckr()
+require('pckr').add{
+    '~/.fzf';
+    'nvim-lualine/lualine.nvim';
+    'echasnovski/mini.starter';
+    'instant-markdown/vim-instant-markdown';
+    'sphamba/smear-cursor.nvim';
+    'metalelf0/black-metal-theme-neovim';
+}
+
