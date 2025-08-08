9a267a8250b884ce8f25e29168001c4cf7efb9ee
freekvb <freekvb@gmail.com>
2025-08-08 10:25:56 +0200

    update

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -46,6 +46,22 @@ vim.opt.complete:append("kspell")
 vim.opt.completeopt = "menu,menuone,noselect,popup,fuzzy"
 vim.o.pumheight = 15
 
+-- auto completion
+local triggers = {'.'}
+vim.api.nvim_create_autocmd('InsertCharPre', {
+  buffer = vim.api.nvim_get_current_buf(),
+  callback = function()
+    if vim.fn.pumvisible() == 1 or vim.fn.state('m') == 'm' then
+      return
+    end
+    local char = vim.v.char
+    if vim.list_contains(triggers, char) then
+      local key = vim.keycode('<C-x><C-n>')
+      vim.api.nvim_feedkeys(key, 'm', false)
+    end
+  end
+})
+
 -- folding (curly brackets)
 vim.o.foldmethod = "marker"
 
